fe42cf9af5dccbe48d091dbdcb38e544d9baa9bb
Mike Austin <mike_ekim@yahoo.com>
2020-05-08 14:44:15 -0700

    Add ability to change position via text input

--- src/components/app/PropertiesPanel.tsx
+++ src/components/app/PropertiesPanel.tsx
@@ -1,54 +1,99 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
 import { StyleSheet, View, Text, TextInput } from 'react-native-web';
 
 import Panel from '../shared/Panel';
 import ShapeContext from '../../ShapeContext';
 import { Spacer, Slider } from '../core';
 
-const NumericInput = ({ value }) => {
-  const styles = StyleSheet.create({
-    numericInput: {
-      backgroundColor: 'white',
-      borderWidth: 1,
-      borderRadius: 4,
-      borderColor: '#c0c0c0',
-      flexDirection: 'row',
-      alignItems: 'center',
-      paddingRight: 5,
-      height: 30,
-    },
-    input: {
-      outlineWidth: 0,
-      width: 40,
-      marginRight: 3,
-      marginTop: -1,
-      textAlign: 'right'
-    }
-  });
+const styles = StyleSheet.create({
+  numericInput: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#c0c0c0',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingRight: 5,
+    height: 30,
+  },
+  input: {
+    outlineWidth: 0,
+    width: 40,
+    marginRight: 3,
+    marginTop: -1,
+    textAlign: 'right'
+  }
+});
 
+const NumericInput = ({ value, ...props }) => {
   return (
     <View style={styles.numericInput}>
-      <TextInput value={value} style={styles.input} />
+      <TextInput value={value} style={styles.input} {...props} />
       <Text>px</Text>
     </View>
   );
 };
 
-const Field = ({ label, value }) => {
+type FieldProps = {
+  label: string,
+  value: any,
+  onChangeText?: Function,
+  onBlur?: Function,
+};
+
+const Field = React.memo(({ label, value, ...props }: FieldProps) => {
+  console.log('Field()', value);
+
   return (
     <View style={{ flexDirection: 'row', alignItems: 'center' }}>
       <Text>{label}</Text>
       <Spacer />
-      <NumericInput value={value} />
+      <NumericInput value={value} {...props} />
     </View>
+  );
+});
+
+type InputFieldProps = {
+  label: string,
+  shape: any,
+  property: string,
+  index: number,
+  dispatch: Function,
+};
+
+const withProperty = property => Component => ({ ...props }) => {
+  return (
+    <Component property={property} {...props} />
   );
 };
 
-const InputField = ({ label, shape, property, index }) => {
+const InputField = React.memo(({ label, shape, property, index, dispatch }: InputFieldProps) => {
+  console.log('InputField()', label);
+
+  const [value, setValue] = useState(shape ? shape[property][index] : 0);
+
+  useEffect(() => {
+    setValue(shape ? shape[property][index] : 0);
+  }, [shape && shape[property]]);
+
+  const handleChangeText = useCallback((text) => setValue(text), []);
+
+  const handleBlur = useCallback(event => {
+    dispatch({
+      type: 'SET_SHAPE_PROPERTY',
+      payload: {
+        shapeId: shape.id,
+        property,
+        index,
+        value: Number(event.nativeEvent.text)
+      }
+    });
+  }, [shape && shape[property]]);
+
   return (
-    <Field label={label} value={shape ? shape[property][index] : 0} />
+    <Field label={label} value={value} onChangeText={handleChangeText} onBlur={handleBlur} />
   );
-};
+});
 
 const PropertiesPanel = ({ selectedShapeId, dispatch, onShapeUpdate }) => {
   console.log('PropertiesPanel()');
@@ -86,7 +131,7 @@ const PropertiesPanel = ({ selectedShapeId, dispatch, onShapeUpdate }) => {
               </View>
               <Spacer />
               <Field label="X" value={selectedShape ? selectedShape.position[0] : 0} />
-              <InputField label="X" shape={selectedShape} property="position" index={0} />
+              <InputField label="X" shape={selectedShape} property="position" index={0} dispatch={dispatch} />
             </>
           )}
         </ShapeContext.Consumer>
